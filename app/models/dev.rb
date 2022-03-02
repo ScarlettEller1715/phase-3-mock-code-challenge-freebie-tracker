eff63b36a0565bfd1ff5dd0825ae6c271b818fcf
Scarlett Eller <rq6715@gmail.com>
2022-03-02 11:44:36 -0500

    all deliverables completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def recieved_one?(item_name)
+        self.freebies.exists?(:item_name => item_name)
+    end
+
+    def give_away(dev, freebie)
+       freebie.update(freebie.id, :dev_id => dev.id)
+    end
+
+
 
 end
